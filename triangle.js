df21fdb4974b562bdaf33792f957facd7ea6d81c
babbu47 <111335500+babbu47@users.noreply.github.com>
2022-10-09 22:59:35 +0530

    text added

---
+++ triangle.js
@@ -0,0 +1,35 @@
+var a1=document.querySelector("#angle1")
+var a2=document.querySelector("#angle2")
+var a3=document.querySelector("#angle3")
+var check=document.querySelector("#buto")
+var text=document.querySelector(".textmessage")
+var ch1=(a1.value).includes("")
+var ch2=(a2.value).includes("")
+var ch3=(a3.value).includes("")
+
+check.addEventListener("click",function checktriangle()
+{
+   if((Number(a1.value)<=0||Number(a2.value)<=0||Number(a3.value)<=0)||(Number(a1.value)>=180||Number(a2.value)>=180||Number(a3.value)>=180))
+   {
+    text.innerText="An angle cannot be less than equal to zero or greater than equal to 180."
+   }
+else
+{
+var sum=Number(a1.value)+Number(a2.value)+Number(a3.value);
+
+if(sum==180)
+{
+    text.innerText="Hurray! the angles you entered forms a triangle."
+}
+else{
+    text.innerText="Alas! the angles don't form a triangle"
+}
+
+}
+
+
+
+
+
+
+});
